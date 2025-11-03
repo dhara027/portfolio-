131f0aa80c5bdbdab305cea93a8f0b84c472f8f9
dhara027 <jadavdhara027@gmail.com>
2025-11-03 09:03:30 -0800

    Initial portfolio project upload

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -36,7 +36,8 @@ const Hero = () => {
               className="bg-gradient-to-r from-primary to-secondary hover:opacity-90 transition-opacity text-white shadow-lg"
             >
               <Download className="w-5 h-5 mr-2" />
-              Download CV
+              Download resume
+            
             </Button>
           </a>
         </div>
